9ac4e638dead821d8868962aa50da59037ae1287
Eugene Ernoult <e.ernoult@gmail.com>
2018-08-14 14:26:28 +0200

    Changed css

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # CREATE
-  get '/tasks/new', to: "tasks#new"
-  post '/tasks', to: "tasks#create"
+  resources :tasks
 
-  # READ
-  get '/tasks', to: "tasks#index"
-  get '/tasks/:id', to: "tasks#show", as: :task
+  # # CREATE
+  # get '/tasks/new', to: "tasks#new"
+  # post '/tasks', to: "tasks#create"
 
-  # UPDATE
-  get '/tasks/:id/edit', to: "tasks#edit", as: :task_edit
-  patch '/tasks/:id', to: "tasks#update"
+  # # READ
+  # get '/tasks', to: "tasks#index"
+  # get '/tasks/:id', to: "tasks#show", as: :task
 
-  # DELETE
-  delete '/tasks/:id', to: "tasks#destroy", as: :task_delete
+  # # UPDATE
+  # get '/tasks/:id/edit', to: "tasks#edit", as: :task_edit
+  # patch '/tasks/:id', to: "tasks#update"
+
+  # # DELETE
+  # delete '/tasks/:id', to: "tasks#destroy", as: :task_delete
 end
